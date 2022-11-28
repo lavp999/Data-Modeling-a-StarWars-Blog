c1ab94794ee21afc0f6b6ca88479c120aa22fbb7
lavp999 <lavp99@hotmail.com>
2022-11-28 19:23:48 +0000

    diagrama

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -8,23 +8,53 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+# 
+# class Person(Base):
+#     __tablename__ = 'person'
+#     # Here we define columns for the table person
+#     # Notice that each column is also a normal Python instance attribute.
+#     id = Column(Integer, primary_key=True)
+#     name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+# class Address(Base):
+#     __tablename__ = 'address'
+#     # Here we define columns for the table address.
+#     # Notice that each column is also a normal Python instance attribute.
+#     id = Column(Integer, primary_key=True)
+#     street_name = Column(String(250))
+#     street_number = Column(String(250))
+#     post_code = Column(String(250), nullable=False)
+#     person_id = Column(Integer, ForeignKey('person.id'))
+#     person = relationship(Person)
+
+class User(Base):
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    username = Column(String(45), nullable=False)
+    email = Column(String(120), nullable=False)
+    password = Column(String(45), nullable=False)
+
+class Character(Base):
+    __tablename__ = 'characters'
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(100), nullable=False)
+
+class Planet(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True)
+    nombre = Column(String(100), nullable=False)
+
+class Favorit(Base):
+    __tablename__ = 'favorits'
+    id = Column(Integer, primary_key=True)
+    id_user = Column(Integer, ForeignKey('users.id'))
+    tipo = Column(Enum, nullable=False)
+    id_character = Column(Integer, ForeignKey('characters.id'), nullable=True)
+    id_planet = Column(Integer, ForeignKey('planets.id'), nullable=True)
+    user = relationship(User)
+    character = relationship(Character)
+    planet = relationship(Planet)
+
 
     def to_dict(self):
         return {}
